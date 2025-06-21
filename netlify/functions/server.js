6951784e904d3f46d0d651a127fb52c319e02689
WTHACD <walejandrocamacho@gmail.com>
2025-06-20 23:26:07 -0600

    156546

--- netlify/functions/server.js
+++ netlify/functions/server.js
@@ -1,26 +1,25 @@
+
+
 const express = require('express');
 const path = require('path');
 const serverless = require('serverless-http');
-const mainRoutes = require('../../routes/index');
 
+const mainRoutes = require('../../routes/index');
 
 require('dotenv').config();
 
 const app = express();
 
-
 app.set('view engine', 'ejs');
+
 app.set('views', path.join(__dirname, '../../views'));
 
 
-app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(express.static(path.join(__dirname, '../../public')));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
 app.use('/', mainRoutes);
-
 
 module.exports.handler = serverless(app);
